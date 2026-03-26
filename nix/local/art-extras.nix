da0031b6af70461157c395cf589e35cd9be44817
Trevor Hanken <trevor.hanken@gmail.com>
2026-03-25 17:18:00 -0700

    refactor: clarify local art package imports and remove redundant wonderdraft package comments.

--- nix/local/art-extras.nix
+++ nix/local/art-extras.nix
@@ -1,4 +1,4 @@
 { pkgs, ... }:
 {
-  home.packages = [ pkgs.wonderdraft ]; # # Need to manually add to nix store: "nix-store --add-fixed sha256 Wonderdraft-1.1.8.2b-Linux64.deb"
+  home.packages = [ pkgs.wonderdraft ]; # Need to manually add to nix store: "nix-store --add-fixed sha256 Wonderdraft-1.1.8.2b-Linux64.deb"
 }

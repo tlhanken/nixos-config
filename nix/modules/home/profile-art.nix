2acafd77e27b2ba1d9f9eacdf4a3cf24ec3f61e5
Trevor Hanken <trevor.hanken@gmail.com>
2026-06-03 19:23:12 -0700

    remove wonderdraft for CI

--- nix/modules/home/profile-art.nix
+++ nix/modules/home/profile-art.nix
@@ -1,10 +1,10 @@
 { pkgs, ... }:
 {
   # Enable or disable this block to turn on or off wonderdraft/other non-public packages
-  imports = 
-    if builtins.pathExists ../../local/art-extras.nix 
-    then [ ../../local/art-extras.nix ] 
-    else [];
+  # imports = 
+  #   if builtins.pathExists ../../local/art-extras.nix 
+  #   then [ ../../local/art-extras.nix ] 
+  #   else [];
 
   # General Packages
   home.packages = with pkgs; [
